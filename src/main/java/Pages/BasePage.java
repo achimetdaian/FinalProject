f1c0fbe0235f4355e4db0df22c9802309ec31d0d
Achimet Emanuel Daian <achimetdaian@yahoo.com>
2023-10-23 22:59:18 -0800

    initial commit

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -29,9 +29,9 @@ public class BasePage {
         driver.get(baseURL);
 
     }
-//    @AfterClass
-//    public void tearDown() {driver.quit();
-//    }
+    @AfterClass
+    public void tearDown() {driver.quit();
+    }
 }
 
 
